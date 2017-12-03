660107eb85a08fb9e5a82b841f96b258ef4008f7
Alex Richardson <alex@alexcr.com>
2017-12-03 14:37:01 -0500

    3 part 2

--- 3/3.js
+++ 3/3.js
@@ -44,8 +44,80 @@ const numSteps = target => {
   return Math.abs(currentPosition[0]) + Math.abs(currentPosition[1]);
 };
 
-console.log(numSteps(14));
-console.log(numSteps(12));
-console.log(numSteps(23));
-console.log(numSteps(1024));
-console.log(numSteps(361527));
+console.log('Part 1', numSteps(361527));
+
+// part 2 - what a terrible problem, and worse solution. brute force.
+
+const getLookupKey = position => `${position[0]},${position[1]}`;
+const getValueOfPosition = (position, grid) => grid[getLookupKey(position)] ? grid[getLookupKey(position)] : 0;
+const getSumOfAdjacentCells = (position, grid) => {
+  let sum = 0;
+
+  sum += getValueOfPosition([position[0] + 1, position[1]], grid);
+  sum += getValueOfPosition([position[0] + 1, position[1] + 1], grid);
+  sum += getValueOfPosition([position[0], position[1] + 1], grid);
+  sum += getValueOfPosition([position[0] - 1, position[1] + 1], grid);
+  sum += getValueOfPosition([position[0] - 1, position[1]], grid);
+  sum += getValueOfPosition([position[0] - 1, position[1] - 1], grid);
+  sum += getValueOfPosition([position[0], position[1] - 1], grid);
+  sum += getValueOfPosition([position[0] + 1, position[1] - 1], grid);
+
+  return sum;
+};
+const getNextPosition = (position, facing) => {
+  if (facing === 'R') {
+    return [position[0] + 1, position[1]];
+  } else if (facing === 'U') {
+    return [position[0], position[1] + 1];
+  } else if (facing === 'L') {
+    return [position[0] - 1, position[1]];
+  } else {
+    return [position[0], position[1] - 1];
+  }
+};
+const getNextDirection = facing => {
+  if (facing === 'R') { return 'U'; }
+  else if (facing === 'U') { return 'L'; }
+  else if (facing === 'L') { return 'D'; }
+  else { return 'R'; }
+};
+
+const findFirstValueLargerInSpiral = target => {
+  const GRID = { '0,0': 1 };
+  let currentPosition = [0, 0];
+  let facing = 'R';
+  let step = 1;
+  let movesInThisDirection = 1;  // have a counter to know how many times we'ves moved in this direction
+  let nextSum = 1;
+  let nextPosition = null;
+
+  // generate the spiral
+  // every two turns we add one to our step limit
+  while (true) {
+    currentPosition = getNextPosition(currentPosition, facing);
+    nextSum = getSumOfAdjacentCells(currentPosition, GRID);
+
+    GRID[getLookupKey(currentPosition)] = nextSum;
+    movesInThisDirection--;
+
+    if (nextSum > target) {
+      break;
+    }
+
+    // we need to change direction
+    if (movesInThisDirection === 0) {
+      facing = getNextDirection(facing);
+      movesInThisDirection = step; // reset our walking counter
+
+      // two turns, add one
+      if (facing === 'L' || facing == 'R') {
+        step++;
+        movesInThisDirection++;
+      }
+    }
+  }
+
+  return nextSum;
+};
+
+console.log('Part 2', findFirstValueLargerInSpiral(361527));
